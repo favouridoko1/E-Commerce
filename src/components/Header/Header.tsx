aa4cd5fc0ade15537154f69cad30f5b64cc2460a
Favour Idoko <ojodomofavour2004@gmail.com>
2024-11-01 12:45:29 +0100

    first commit

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="w-[80%] rounded border-2 border-[#a5a]">
+      <p className='text-3xl'>Header</p>
+    </div>
+  );
+};
+
+export default Header;
